bf9e05a22b74c2d98451bb7c966f037daf4022c5
willhammer <vadimwillhammer@gmail.com>
2018-03-25 23:25:26 +0300

    Create loadpy27.bat

---
+++ loadpy27.bat
@@ -0,0 +1,2 @@
+setx PYTHONHOME Z:/PythonAnaconda/envs/py27
+setx PYTHONPATH %PYTHONHOME%/Scripts;%PYTHONHOME%/DLLs;%PYTHONHOME%/Lib;%PYTHONHOME%/Lib/lib-tk;%PYTHONHOME%/Library/bin;%PYTHONHOME%/Library/usr/bin;%PYTHONHOME%/Library/mingw-w64/bin;%PYTHONHOME%/bin;
