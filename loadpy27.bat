a545c9784870d052c68b7a3348a9d0c485dc58e2
willhammer <vadimwillhammer@gmail.com>
2018-03-26 00:11:06 +0300

    Update loadpy27.bat

--- loadpy27.bat
+++ loadpy27.bat
@@ -1,2 +1,4 @@
 setx PYTHONHOME Z:/PythonAnaconda/envs/py27
 setx PYTHONPATH %PYTHONHOME%/Scripts;%PYTHONHOME%/DLLs;%PYTHONHOME%/Lib;%PYTHONHOME%/Lib/lib-tk;%PYTHONHOME%/Library/bin;%PYTHONHOME%/Library/usr/bin;%PYTHONHOME%/Library/mingw-w64/bin;%PYTHONHOME%/bin;
+start cmd
+exit
